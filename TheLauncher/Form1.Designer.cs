3fb66874b4f494c17806fc2705afe3a535c62bdb
MindUrOwnBusiness <sygamersy@gmail.com>
2019-02-22 10:17:59 +0200

    Maximizing error fix

--- TheLauncher/Form1.Designer.cs
+++ TheLauncher/Form1.Designer.cs
@@ -37,11 +37,11 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.HomePage = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.HomePage = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel12.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -162,6 +162,54 @@
             this.panel1.Size = new System.Drawing.Size(1097, 384);
             this.panel1.TabIndex = 8;
             // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Unispace", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(856, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 58);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Servers";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Unispace", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(763, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(286, 58);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Minecraft";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Unispace", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(62, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 58);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gaming";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // HomePage
+            // 
+            this.HomePage.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.HomePage.AutoSize = true;
+            this.HomePage.Font = new System.Drawing.Font("Unispace", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomePage.Location = new System.Drawing.Point(12, 34);
+            this.HomePage.Name = "HomePage";
+            this.HomePage.Size = new System.Drawing.Size(286, 58);
+            this.HomePage.TabIndex = 1;
+            this.HomePage.Text = "Streaming";
+            this.HomePage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // pictureBox1
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -174,58 +222,6 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // HomePage
-            // 
-            this.HomePage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.HomePage.AutoSize = true;
-            this.HomePage.Font = new System.Drawing.Font("Unispace", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HomePage.Location = new System.Drawing.Point(12, 34);
-            this.HomePage.Name = "HomePage";
-            this.HomePage.Size = new System.Drawing.Size(286, 58);
-            this.HomePage.TabIndex = 1;
-            this.HomePage.Text = "Streaming";
-            this.HomePage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Unispace", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(62, 280);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(199, 58);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Gaming";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label2
-            // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Unispace", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(763, 90);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(286, 58);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Minecraft";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label3
-            // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Unispace", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(856, 194);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(228, 58);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Servers";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
